6e76082545129b5a985dcae5d307a11e25913df3
lehlohonolo.tladi@sambeconsulting.com <lehlohonolo.tladi@sambeconsulting.com>
2023-02-14 14:34:53 +0200

    few changes

---
+++ Program_Flow_Control/WalrusOperator.py
@@ -0,0 +1,7 @@
+# The colon equals operator
+# It evuluates an expression then assign a value
+while (x := input("Enter:")) != '':
+    print(x)
+# Evaluates 1st item in the list and its last digit
+l1 = ['python', '[1,2]', 'java1', 30, [2, 3]]
+print(l1[0][-1])
