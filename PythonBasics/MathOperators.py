71992d6fb3fc1b373521abc98e93b2ff1bd24b8f
lehlohonolo.tladi@sambeconsulting.com <lehlohonolo.tladi@sambeconsulting.com>
2023-02-15 09:44:55 +0200

    Fixed naming conventions

---
+++ PythonBasics/MathOperators.py
@@ -0,0 +1,18 @@
+a = 9
+b = 2
+
+# YOUR CODE STARTS HERE:
+
+# c equals a divided by b
+c = a / b
+
+# d equals a raised to the power of b
+d = a ** b
+
+# e equals the remainder of a divided by b
+e = a % b
+
+# f equals a multiplied by b
+f = a * b
+
+print("This are basic operators")
