e98da4fc23e8bada5a3e51ecd22553bcd53938b9
Lehlohonolo Tladi <lehlohoonolo.tladi@sambeconsulting.com>
2023-02-17 16:02:47 +0200

    OOP

--- WorkingWithTextFiles/TextFilesExercises.py
+++ WorkingWithTextFiles/TextFilesExercises.py
@@ -3,3 +3,9 @@
 
 #Relative path
 #file = open(r'C:\report.txt')
+
+#The code below opens a file called ip.txt
+#I will comment is out bceause I was doing the exercises and ip.txt doesnt exist
+#Reads the file in a list called ip_list
+#with open('ip.txt') as f:
+#    ip_list = f.read().splitlines()
