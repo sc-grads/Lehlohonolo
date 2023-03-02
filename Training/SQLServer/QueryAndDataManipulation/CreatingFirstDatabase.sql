723ccca157374f7c759315d09c2193160adec095
Lehlohonolo Tladi <lehlohoonolo.tladi@sambeconsulting.com>
2023-03-02 15:45:03 +0200

    Query and Data Manipulation

---
+++ Training/SQLServer/QueryAndDataManipulation/CreatingFirstDatabase.sql
@@ -0,0 +1,17 @@
+USE [MyFirstDatabase]
+GO
+INSERT INTO [dbo].[PersonalInfo]
+           ([FirstName]
+           ,[LastName]
+           ,[DateOfBirth]
+           ,[ID])
+     VALUES
+           ('Lehlohonolo',
+           'Tladi',
+           '01-01-2023'
+           ,777)
+GO
+
+select * from [dbo].[PersonalInfo]
+
+
